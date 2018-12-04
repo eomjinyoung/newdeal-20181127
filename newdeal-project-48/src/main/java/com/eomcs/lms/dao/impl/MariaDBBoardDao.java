762e461598a003acbd63a891d0794a51beb488f4
eomjinyoung <jinyoung.eom@gmail.com>
2018-12-04 12:56:18 +0900

    'v48'

--- newdeal-project-48/src/main/java/com/eomcs/lms/dao/impl/MariaDBBoardDao.java
+++ newdeal-project-48/src/main/java/com/eomcs/lms/dao/impl/MariaDBBoardDao.java
@@ -2,8 +2,8 @@ package com.eomcs.lms.dao.impl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import org.mariadb.jdbc.Driver;
@@ -19,9 +19,9 @@ public class MariaDBBoardDao implements BoardDao {
         Connection con = DriverManager.getConnection(
         "jdbc:mariadb://localhost:3306/studydb", 
         "study", "1111");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(
-            "select bno, cont, cdt, view from board");) {
+        PreparedStatement stmt = con.prepareStatement(
+            "select bno, cont, cdt, view from board");
+        ResultSet rs = stmt.executeQuery();) {
       
       ArrayList<Board> list = new ArrayList<>();
       
@@ -45,23 +45,24 @@ public class MariaDBBoardDao implements BoardDao {
         Connection con = DriverManager.getConnection(
             "jdbc:mariadb://localhost:3306/studydb", 
             "study", "1111");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(
+        PreparedStatement stmt = con.prepareStatement(
             "select bno, cont, cdt, view, mno, lno"
-            + " from board"
-            + " where bno=" + no);) {
+                + " from board where bno=?");) {
       
-      if (rs.next()) {
-        Board board = new Board();
-        board.setNo(rs.getInt("bno"));
-        board.setContents(rs.getString("cont"));
-        board.setCreatedDate(rs.getDate("cdt"));
-        board.setViewCount(rs.getInt("view"));
-        board.setWriterNo(rs.getInt("mno"));
-        board.setLessonNo(rs.getInt("lno"));
-        return board;
-      } else {
-        return null;
+      stmt.setInt(1, no);
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (rs.next()) {
+          Board board = new Board();
+          board.setNo(rs.getInt("bno"));
+          board.setContents(rs.getString("cont"));
+          board.setCreatedDate(rs.getDate("cdt"));
+          board.setViewCount(rs.getInt("view"));
+          board.setWriterNo(rs.getInt("mno"));
+          board.setLessonNo(rs.getInt("lno"));
+          return board;
+        } else {
+          return null;
+        }
       }
     }
   }
@@ -72,31 +73,32 @@ public class MariaDBBoardDao implements BoardDao {
         Connection con = DriverManager.getConnection(
         "jdbc:mariadb://localhost:3306/studydb", 
         "study", "1111");
-        Statement stmt = con.createStatement();) {
+        PreparedStatement stmt = con.prepareStatement(
+            "insert into board(cont,mno,lno) values(?,?,?)");) {
 
-      // SQL을 서버에 전송 
-      return stmt.executeUpdate("insert into board(cont,mno,lno)"
-          + " values('" + board.getContents() + "',"
-          + board.getWriterNo() + ","
-          + board.getLessonNo() + ")");
+      stmt.setString(1, board.getContents());
+      stmt.setInt(2, board.getWriterNo());
+      stmt.setInt(3, board.getLessonNo());
+      
+      return stmt.executeUpdate();
     }
   }
   
   public int update(Board board) throws Exception {
     Connection con = null;
-    Statement stmt = null;
+    PreparedStatement stmt = null;
     
     try {
       DriverManager.registerDriver(new Driver());
       con = DriverManager.getConnection(
           "jdbc:mariadb://localhost:3306/studydb", 
           "study", "1111");
-      stmt = con.createStatement();
+      stmt = con.prepareStatement(
+          "update board set cont=? where bno=?");
       
-      return stmt.executeUpdate(
-          "update board set cont='" + 
-          board.getContents() + 
-          "' where bno=" + board.getNo());
+      stmt.setString(1, board.getContents());
+      stmt.setInt(2, board.getNo());
+      return stmt.executeUpdate();
       
     } finally {
       try {stmt.close();} catch (Exception e) {}
@@ -106,16 +108,16 @@ public class MariaDBBoardDao implements BoardDao {
   
   public int delete(int no) throws Exception {
     Connection con = null;
-    Statement stmt = null;
+    PreparedStatement stmt = null;
     
     try {
       DriverManager.registerDriver(new Driver());
       con = DriverManager.getConnection(
           "jdbc:mariadb://localhost:3306/studydb", 
           "study", "1111");
-      stmt = con.createStatement();
-
-      return stmt.executeUpdate("delete from board where bno=" + no);
+      stmt = con.prepareStatement("delete from board where bno=?");
+      stmt.setInt(1, no);
+      return stmt.executeUpdate();
       
     } finally {
       try {stmt.close();} catch (Exception e) {}
--- newdeal-project-48/src/main/java/com/eomcs/lms/dao/impl/MariaDBMemberDao.java
+++ newdeal-project-48/src/main/java/com/eomcs/lms/dao/impl/MariaDBMemberDao.java
@@ -2,8 +2,8 @@ package com.eomcs.lms.dao.impl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import org.mariadb.jdbc.Driver;
 import com.eomcs.lms.dao.MemberDao;
 import com.eomcs.lms.domain.Member;
@@ -17,22 +17,24 @@ public class MariaDBMemberDao implements MemberDao {
         Connection con = DriverManager.getConnection(
             "jdbc:mariadb://localhost:3306/studydb", 
             "study", "1111");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(
-            "select mno,name,email,photo"
-            + " from member"
-            + " where email='" + email 
-            + "' and pwd='" + password + "'");) {
+        PreparedStatement stmt = con.prepareStatement(
+            "select mno,name,email,photo from member"
+            + " where email=? and pwd=?");) {
       
-      if (rs.next()) {
-        Member member = new Member();
-        member.setNo(rs.getInt("mno"));
-        member.setName(rs.getString("name"));
-        member.setEmail(rs.getString("email"));
-        member.setPhoto(rs.getString("photo"));
-        return member;
-      } else {
-        return null;
+      stmt.setString(1, email);
+      stmt.setString(2, password);
+      
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (rs.next()) {
+          Member member = new Member();
+          member.setNo(rs.getInt("mno"));
+          member.setName(rs.getString("name"));
+          member.setEmail(rs.getString("email"));
+          member.setPhoto(rs.getString("photo"));
+          return member;
+        } else {
+          return null;
+        }
       }
     }
   }
